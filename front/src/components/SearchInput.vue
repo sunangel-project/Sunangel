749abdd90bee415c9dccff5d8d675549ec6c4696
Fabian Weik <fabian.weik@protonmail.com>
2023-05-22 22:31:15 +0200

    add simple vue frontend

---
+++ front/src/components/SearchInput.vue
@@ -0,0 +1,39 @@
+<script lang="ts">
+import searching from '../searching'
+
+export default {
+    data() {
+        return {
+            lat: 48.74517,
+            lon: 9.10666,
+            radius: 2000,
+        }
+    },
+    methods: {
+        search: function () {
+            searching.search(this.lat, this.lon, this.radius)
+        }
+    }
+}
+</script>
+
+<template>
+    <h2>Search Input</h2>
+
+    <div>
+        Latitude:
+        <input v-model="lat">
+    </div>
+
+    <div>
+        Longitude:
+        <input v-model="lon">
+    </div>
+
+    <div>
+        Search Radius:
+        <input v-model="radius" type="number">
+    </div>
+
+    <button @click="search">Search</button>
+</template>
---
+++ front/src/searching.ts
@@ -0,0 +1,5 @@
+export default {
+    search: function (lat: number, lon: number, radius: number) {
+        console.log(lat, lon, radius)
+    }
+}
