85d1b626d2a03bf50c5317fab2ba851f86ed620a
Fabian Weik <fabian.weik@protonmail.com>
2023-10-03 22:09:07 +0200

    implemented algorithm for ecliptic coordinates of moon

--- sky-service/src/sky/moon.rs
+++ sky-service/src/sky/moon.rs
@@ -1,8 +1,24 @@
+use std::f64::consts::PI;
+
 use chrono::{Duration, NaiveDateTime};
 
-use crate::{julian, Location, SkyObject, SkyPosition};
+use crate::{angle::AngleExtensions, julian, Location, SkyObject, SkyPosition};
 
 use super::util;
+
+const LONG_ASC_NODE_0: f64 = 125.1228f64 * PI / 180.;
+const LONG_ASC_NODE_1: f64 = 0.0529538083 * PI / 180.;
+
+const INCLINATION: f64 = 5.1454 * PI / 180.;
+const MEAN_DISTANCE: f64 = 60.2666;
+const ECCENTRICITY: f64 = 0.0549;
+const ECCENTRICITY_EPS: f64 = 0.005 * PI / 180.;
+
+const ARG_OF_PERIGEE_0: f64 = 318.0634 * PI / 180.;
+const ARG_OF_PERIGEE_1: f64 = 0.1643573223 * PI / 180.;
+
+const MEAN_ANOMALY_0: f64 = 115.3654 * PI / 180.;
+const MEAN_ANOMALY_1: f64 = 13.0649929509 * PI / 180.;
 
 pub struct Moon;
 impl SkyObject for Moon {
@@ -11,12 +27,46 @@ impl SkyObject for Moon {
     }
 
     // source: http://www.stjarnhimlen.se/comp/tutorial.html#7
+    #[allow(non_snake_case)]
     fn position(&self, time: &NaiveDateTime, location: &Location) -> SkyPosition {
-        // ecliptic coordinates
         let d = julian::day_of(time) - 2451543.5;
 
-        let alpha = (308.3616f64).to_radians();
-        let delta = (-0.3937f64).to_radians();
+        let N = (LONG_ASC_NODE_0 - LONG_ASC_NODE_1 * d).normalize_radians();
+        let w = (ARG_OF_PERIGEE_0 + ARG_OF_PERIGEE_1 * d).normalize_radians();
+        let M = (MEAN_ANOMALY_0 + MEAN_ANOMALY_1 * d).normalize_radians();
+
+        // Eccentric anomaly
+        let mut E0 =
+            (M + ECCENTRICITY * M.sin() * (1. + ECCENTRICITY * M.cos())).normalize_radians();
+        loop {
+            let E1 = (E0 - (E0 - ECCENTRICITY * E0.sin() - M) / (1. - ECCENTRICITY * E0.cos()))
+                .normalize_radians();
+
+            let delta = (E0 - E1).abs();
+            E0 = E1;
+            if delta < ECCENTRICITY_EPS {
+                break;
+            }
+        }
+        let E = E0;
+
+        // Rectangular coordinates
+        let x = MEAN_DISTANCE * (E.cos() - ECCENTRICITY);
+        let y = MEAN_DISTANCE * (1. - ECCENTRICITY.powi(2)).sqrt() * E.sin();
+
+        // Distance and true anomaly
+        let r = (x.powi(2) + y.powi(2)).sqrt();
+        let v = y.atan2(x);
+
+        // Intermediate ecliptic coordinates
+        let vwsin = (v + w).sin();
+        let xeclip = r * (N.cos() * (v + w).cos() - N.sin() * vwsin * INCLINATION.cos());
+        let yeclip = r * (N.sin() * (v + w).cos() + N.cos() * vwsin * INCLINATION.cos());
+        let zeclip = r * vwsin * INCLINATION.sin();
+
+        // Ecliptic coordinates
+        let alpha = yeclip.atan2(xeclip);
+        let delta = zeclip.atan2((xeclip.powi(2) + yeclip.powi(2)).sqrt());
 
         let (altitude, azimuth) =
             util::convert_ecliptic_to_horizontal(time, location, alpha, delta);
