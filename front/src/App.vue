9b4a8fbc3b57932d8009ffb0e777c7257da0500e
Fabian Weik <fabian.weik@protonmail.com>
2023-10-09 16:20:56 +0200

    display error when backend is not reachable

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,24 +1,5 @@
-<script setup lang="ts">
-import SearchInput from './components/SearchInput.vue'
-import SpotList from './components/Results/SpotList.vue'
-import Map from './components/Map.vue'
-
-import { setupSpotsSubscription } from './searching';
-setupSpotsSubscription()
-
-/* Modal Pop-up
-// Disclaimer
-import { ModalsContainer, useModal } from 'vue-final-modal'
-import Disclaimer from './components/Disclaimer.vue'
-const { open } = useModal({
-    component: Disclaimer,
-})
-open();
-*/
-</script>
-
 <template>
-    <div class="bg-gray-900 text-gray-100 h-screen grid grid-rows-[45%_1fr] md:grid-rows-1 md:grid-cols-[70%_1fr]">
+    <div class="h-screen grid grid-rows-[45%_1fr] md:grid-rows-1 md:grid-cols-[70%_1fr]">
         <div>
             <Map />
         </div>
@@ -28,5 +9,60 @@ open();
         </div>
     </div>
 
-    <!--ModalsContainer /-->
+    <ModalsContainer />
 </template>
+
+<script setup lang="ts">
+import SearchInput from './components/SearchInput.vue'
+import SpotList from './components/Results/SpotList.vue'
+import Map from './components/Map.vue'
+
+import { setupSpotsSubscription } from './searching';
+import { Client, provideClient } from '@urql/vue';
+import { SubscriptionClient } from 'subscriptions-transport-ws';
+import { cacheExchange, fetchExchange, subscriptionExchange } from '@urql/vue';
+
+import { ModalsContainer, useModal } from 'vue-final-modal'
+import Popup from './components/Popup.vue'
+
+const displayConnectionError = () => {
+    const { open } = useModal({
+        component: Popup,
+        attrs: {
+            title: "Error",
+            message: "Backend is not reachable. Please try again later.",
+        },
+    });
+    open();
+};
+
+
+let protocol = "ws";
+let apiHost = "localhost";
+apiHost = "192.168.2.123";
+if (process.env.NODE_ENV == "production") {
+    protocol = "wss";
+    apiHost = "sunnapi.cloudsftp.de";
+}
+
+const subscriptionClient = new SubscriptionClient(
+    `${protocol}://${apiHost}:6660/subscriptions`,
+    { reconnect: false },
+);
+subscriptionClient.client.onerror = displayConnectionError;
+
+const client = new Client({
+    url: `http://${apiHost}:6660/graphql`,
+    exchanges: [
+        cacheExchange,
+        fetchExchange,
+        subscriptionExchange({
+            forwardSubscription: (request) => subscriptionClient.request(request),
+        }),
+    ],
+});
+
+provideClient(client);
+
+setupSpotsSubscription()
+</script>
--- front/src/main.ts
+++ front/src/main.ts
@@ -2,41 +2,12 @@ import { createApp } from 'vue'
 import OpenLayersMap from 'vue3-openlayers';
 import 'vue3-openlayers/dist/vue3-openlayers.css';
 import 'vue-final-modal/style.css'
-import urql, { cacheExchange, fetchExchange, subscriptionExchange } from '@urql/vue';
-
 
 import App from './App.vue';
-// import './assets/main.css';
 import './index.css';
-import { SubscriptionClient } from 'subscriptions-transport-ws';
 import { createVfm } from 'vue-final-modal'
-
-let protocol = "ws";
-let apiHost = "localhost";
-apiHost = "192.168.2.123";
-if (process.env.NODE_ENV == "production") {
-    protocol = "wss";
-    apiHost = "sunnapi.cloudsftp.de";
-}
-
-// TODO: handle connection fail
-// TODO: url as extra variable
-const subscriptionClient = new SubscriptionClient(
-    `${protocol}://${apiHost}:6660/subscriptions`,
-    { reconnect: false },
-);
 
 const app = createApp(App);
 app.use(OpenLayersMap);
-app.use(urql, {
-    url: `http://${apiHost}:6660/graphql`,
-    exchanges: [
-        cacheExchange,
-        fetchExchange,
-        subscriptionExchange({
-            forwardSubscription: (request) => subscriptionClient.request(request),
-        }),
-    ],
-});
 app.use(createVfm());
 app.mount('#app');
