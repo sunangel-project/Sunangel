9f2a408d888383961021d1e02578ff81799a557b
Fabian Weik <fabian.weik@protonmail.com>
2023-11-05 15:53:18 +0100

    add data collected by Akamai

--- front/src/privacy/Privacy.vue
+++ front/src/privacy/Privacy.vue
@@ -99,7 +99,6 @@
                 related to your computer and internet connection.
                 We list them here again for your convenience:
             </div>
-
             <ul class="list-disc list-inside">
                 <li>Browser type and version</li>
                 <li>Operating system used</li>
@@ -107,7 +106,6 @@
                 <li>Website that you visit or request that you make</li>
                 <li>Date and time of access</li>
                 <li>Your Internet Protocol (IP) address</li>
-                <li>Input data of the search form if the search button is pressed</li>
             </ul>
 
             <div>
@@ -145,13 +143,80 @@
                 You can learn more about this
                 <a
                     href="https://docs.github.com/en/site-policy/privacy-policies/github-privacy-statement#cross-border-data-transfers">
-                    here
-                </a>.
+                    here.
+                </a>
                 You can also read about how to dispute the processing of your data by GitHub on that page.
             </div>
 
+            <h3 class="font-bold text-xl">3.2 Akamai</h3>
+
+            <div>
+                We host the backend of this website on <a href="https://www.linode.com/">Linode</a> servers.
+                It is a hosting service owned by <a href="https://www.akamai.com">Akamai</a>.
+                Therefore, Akamai has access to the pieces of data listed above
+                related to your computer and internet connection.
+                We list them here again for your convenience:
+            </div>
+
             <ul class="list-disc list-inside">
-                <li>Linode, ip etc</li>
+                <li>Browser type and version</li>
+                <li>Operating system used</li>
+                <li>Website from which you visit this website (referrer URL)</li>
+                <li>Website that you visit or request that you make</li>
+                <li>Date and time of access</li>
+                <li>Your Internet Protocol (IP) address</li>
+            </ul>
+
+            <div>
+                The data is transmitted to Akamai as soon as you click the "search" button.
+                The legal basis for using this hosting servive is in accordance with Art. 6 Para. 1 lit. f GDPR.
+                The legitimate interest being that we do not want to run a server to host the backend of the website
+                on a server in our facilities.
+            </div>
+
+            <div>
+                The servers we rent from this service are located in Frankfurt, Germany.
+                Although we only use servers located in Europe, Akamai is an american company
+                and its headquarters are located at:
+            </div>
+
+            <div>
+                Akamai Technologies Inc. <br>
+                150 Broadway <br>
+                Cambridge, MA 021142 <br>
+                United States
+            </div>
+
+            <div>
+                The data will be deleted as soon as the purpose of processing no longer applies.
+                Akamais's purpose for processing the data is the following:
+            </div>
+
+            <ul class="list-disc list-inside">
+                <li>Guarantee functionality of their IT systems and the technology of their hosting service</li>
+            </ul>
+
+            <div>
+                Akamai implemented compliance measures for cross-border data transfers.
+                Therefore, they act in accordance to the GDPR.
+                You can learn more about this
+                <a href="https://www.akamai.com/legal/compliance/privacy-trust-center/cross-border-data-transfer-statement">
+                    here.
+                </a>
+            </div>
+
+            <div>
+                The input to your search query is encrypted by your browser when making the request
+                and only decrypted on the server.
+                Therefore, Akamai does not have access to this data directly.
+                But it is stores on the servers in log files and therefore processed by Akamai
+                by storing the data on their infrastructure.
+                Further processing of this data is not done by Akamai.
+            </div>
+
+            <h3 class="font-bold text-xl">3.3 Openstreesmaps</h3>
+
+            <ul class="list-disc list-inside">
                 <li>Openstreetmaps, search input minus ip etc</li>
             </ul>
         </div>
