3bc28b7e40ffae6e2a4ab7b845c435654346f3cf
Fabian Weik <fabian.weik@protonmail.com>
2024-04-09 18:30:30 +0200

    attempt image push

--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -1,14 +1,34 @@
 #!/usr/bin/env bash
 
-SERVICES="api spot-finder horizon-get horizon-compute sky-service"
+push="false"
+if [ "$1" = "--push" ]; then
+    push="true"
+
+    user="$2"
+    password="$3"
+    tag="latest"
+fi
+
+services="api spot-finder horizon-get horizon-compute sky-service"
 
 function build-image() {
-    IMAGE_NAME="$1"
-    docker build . -f "Dockerfiles/${IMAGE_NAME}" -t "${IMAGE_NAME}"
+    image_name="$1"
+    docker build . -f "Dockerfiles/$image_name" -t "$image_name:$tag"
 }
 
 build-image sunangel-rust-base
 
-for service in ${SERVICES}; do
+for service in $services; do
     build-image "${service}"
+done
+
+if [ "$push" = "false" ]; then
+    exit 0
+fi
+
+docker login ghcr.io --username "$user" --password "$password"
+
+for service in $services; do
+    docker tag "$service" "ghcr.io/$user/$service:$tag"
+    docker push "ghcr.io/$user/$service:$tag"
 done
