ef18ed3b4a8a0b50260076cc4793124d45ee755c
Fabian Weik <fabian.weik@protonmail.com>
2024-04-09 17:55:34 +0200

    also build sky-service with chef

--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
-TAG="latest"
-
 SERVICES="api spot-finder horizon-get horizon-compute sky-service"
 
 function build-image() {
     IMAGE_NAME="$1"
-    docker build . -f "Dockerfiles/${IMAGE_NAME}" -t "${IMAGE_NAME}:${TAG}"
+    docker build . -f "Dockerfiles/${IMAGE_NAME}" -t "${IMAGE_NAME}"
 }
 
 build-image sunangel-rust-base
