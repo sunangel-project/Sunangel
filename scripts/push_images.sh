e218a1a3ec96fecb85ef2e045b944d022beafff6
Fabian Weik <fabian.weik@protonmail.com>
2024-04-11 21:29:12 +0200

    rename images before pushing

--- scripts/push_images.sh
+++ scripts/push_images.sh
@@ -9,6 +9,7 @@ password="$3"
 echo $password | docker login ghcr.io --username "$user" --password-stdin
 
 for service in $services; do
+    docker tag "$service" "ghcr.io/sunangel-project/$service"
     docker push "ghcr.io/sunangel-project/$service:$version"
     docker push "ghcr.io/sunangel-project/$service:latest"
 done
