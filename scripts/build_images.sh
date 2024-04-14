012d3c2e8528175bbe332dd5921af99873558912
Fabian Weik <fabian.weik@protonmail.com>
2024-04-14 10:20:52 +0200

    build and push api image with certificate

--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -3,12 +3,23 @@
 source ./scripts/services.sh
 
 function build-image() {
+    file_name="$1"
     image_name="$1"
     docker build . -f "Dockerfiles/$image_name" -t "$image_name"
 }
 
 build-image sunangel-rust-base
 
+# new certificate for localhost
+openssl req -x509 -newkey rsa:4096 -nodes -keyout key.pem -out cert.pem -days 365 -subj '/CN=localhost'
+
 for service in $services; do
-    build-image "${service}"
+    build-image "$service" "$service"
 done
+
+if [ -n "$CLOUDSFTP_CERT_KEY" ] && [ -n "$CLOUDSFTP_CERT" ]; then
+    echo "$CLOUDSFTP_CERT_KEY" > key.pem
+    echo "$CLOUDSFTP_CERT" > cert.pem
+
+    build-image "api" "api-cloudsftp"
+fi
