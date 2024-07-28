89b487ceeda07ec35d9e9d1885f90ddb785ae2cd
Fabian Weik <fabian.weik@protonmail.com>
2024-07-28 14:00:48 +0200

    update version

--- api/src/api.rs
+++ api/src/api.rs
@@ -47,7 +47,7 @@ impl Context {
 pub struct Query;
 
 const API_VERSION: &str = "v0.2.0";
-const BACKEND_VERSION: &str = "v0.2.4";
+const BACKEND_VERSION: &str = "v0.2.6";
 
 #[graphql_object(context = Context)]
 impl Query {
