6d491fb7e08eb20b5568c0a30d60dd422271cfbc
Fabian Weik <fabian.weik@protonmail.com>
2026-04-03 09:29:22 +0200

    use merge-dirs from daggerverse

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.25.0
 
 require (
 	github.com/joho/godotenv v1.5.1
-	github.com/nats-io/nats.go v1.50.0
+	github.com/nats-io/nats.go v1.51.0
 	github.com/satori/go.uuid v1.2.0
 	github.com/sirupsen/logrus v1.9.4
 	github.com/sunangel-project/horizon v0.0.2
--- pipelines/update.go
+++ pipelines/update.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
 
 	"dagger/sunangel/internal/dagger"
 )
@@ -48,7 +47,6 @@ func updateTypescript(
 		})
 }
 
-// TODO: also test before merging
 type directoryGenerator = func(context.Context, *dagger.Directory) *dagger.Directory
 
 // Update all dependencies
@@ -64,91 +62,13 @@ func (m *Sunangel) UpdateDependencies(
 		updateGo,
 	} {
 		dir := f(ctx, source)
-		if dir == nil { // TODO: how to handle, error?
+		if dir == nil {
 			continue
 		}
 
 		dirs = append(dirs, dir)
 	}
 
-	rustDir, err := mergeDirectories(ctx, dirs)
-	if err != nil {
-		return nil, fmt.Errorf("could not merge directories: %w", err)
-	}
-
-	return rustDir, nil
-}
-
-func mergeDirectories(
-	ctx context.Context,
-	dirs []*dagger.Directory,
-) (*dagger.Directory, error) {
-	var err error
-
-	if len(dirs) < 2 {
-		return nil, fmt.Errorf("need at least 2 directories to merge")
-	}
-
-	first, rest := dirs[0], dirs[1:]
-	for i, next := range rest {
-		first, err = mergeDirectories2(ctx, first, next)
-		if err != nil {
-			return nil, fmt.Errorf("could not merge direcotory %d: %w", i, err)
-		}
-	}
-
-	return first, nil
-}
-
-func mergeDirectories2(
-	ctx context.Context,
-	first *dagger.Directory,
-	second *dagger.Directory,
-) (*dagger.Directory, error) {
-	entries, err := second.Entries(ctx)
-	if err != nil {
-		return nil, fmt.Errorf("could not get entries from second directory: %w", err)
-	}
-
-	for _, path := range entries {
-		first, err = copyPath(ctx, first, second, path)
-		if err != nil {
-			return nil, fmt.Errorf("could not copy at path '%s': %w", path, err)
-		}
-	}
-
-	return first, nil
-}
-
-func copyPath(
-	ctx context.Context,
-	first *dagger.Directory,
-	second *dagger.Directory,
-	path string,
-) (*dagger.Directory, error) {
-	fileType, err := second.Stat(path).FileType(ctx)
-	if err != nil {
-		return nil, fmt.Errorf("could not get file type of path: %w", err)
-	}
-
-	switch fileType {
-	case dagger.FileTypeDirectory:
-		directory := second.Directory(path)
-		first = first.WithDirectory(path, directory)
-
-	case dagger.FileTypeRegular:
-		file := second.File(path)
-		first = first.WithFile(path, file)
-
-	case dagger.FileTypeSymlink:
-		return nil, fmt.Errorf("symlink at")
-
-	case dagger.FileTypeUnknown:
-		return nil, fmt.Errorf("unknown file type")
-
-	default:
-		return nil, fmt.Errorf("unexpected file type: %#v", fileType)
-	}
-
-	return first, nil
+	merged := dag.MergeDirs().MergeDirectories(dirs)
+	return merged, nil
 }
